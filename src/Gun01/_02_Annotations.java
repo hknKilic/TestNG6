cdc4fb9c91035f12128d0af83a829f0badbb6037
TechnoStudy <ismettechno@gmail.com>
2023-08-28 19:38:53 +0100

    testNG _02_Annotations 1

---
+++ src/Gun01/_02_Annotations.java
@@ -0,0 +1,18 @@
+package Gun01;
+
+import org.testng.annotations.Test;
+
+public class _02_Annotations {
+
+    @Test // Annotation : metdoun türünü ve çalışma özelliklerini belirtir
+    public void test1(){  // Test1 : test metodunun adı
+        System.out.println("test 1 çalıştı");
+    }
+
+    @Test // Annotation : metdoun türünü ve çalışma özelliklerini belirtir
+    public void test2(){  // Test1 : test metodunun adı
+        System.out.println("test 2 çalıştı");
+    }
+
+
+}
