b072f762fc69689e9aee3120af1cbf7e3f11f6a0
TechnoStudy <ismettechno@gmail.com>
2023-08-28 19:42:42 +0100

    testNG _02_Annotations 2

--- src/Gun01/_02_Annotations.java
+++ src/Gun01/_02_Annotations.java
@@ -1,17 +1,29 @@
 package Gun01;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class _02_Annotations {
 
-    @Test // Annotation : metdoun türünü ve çalışma özelliklerini belirtir
+    @BeforeClass  // class ın içindekilerden önce çalışır
+    public void baslangicIslemleri(){
+        System.out.println("before class işlemleri çalıştı");
+    }
+
+    @Test // Annotation : metodun türünü ve çalışma özelliklerini belirtir
     public void test1(){  // Test1 : test metodunun adı
         System.out.println("test 1 çalıştı");
     }
 
-    @Test // Annotation : metdoun türünü ve çalışma özelliklerini belirtir
+    @Test // Annotation : metodun türünü ve çalışma özelliklerini belirtir
     public void test2(){  // Test1 : test metodunun adı
         System.out.println("test 2 çalıştı");
+    }
+
+    @AfterClass // class ın içindekilerden sonra çalışır
+    public void bitisIslemleri(){
+        System.out.println("after class işlemleri çalıştı");
     }
 
 
