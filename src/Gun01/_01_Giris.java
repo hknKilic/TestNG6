5b9c7f2962452abe8a8ee255f82dde334ac6c17c
TechnoStudy <ismettechno@gmail.com>
2023-08-28 19:23:24 +0100

    testNG Giriş 1

--- src/Gun01/_01_Giris.java
+++ src/Gun01/_01_Giris.java
@@ -1,4 +1,42 @@
 package Gun01;
 
+
+import org.testng.annotations.Test;
+
 public class _01_Giris {
+
+//    public static void main(String[] args) {
+//        driverIslemleriniYap();
+//        websitesiniAc();
+//        LoginTestiniYap()
+//    }
+
+//      @Test JUnit Test i
+//      public void Test1()
+//      {
+//          driverIslemleriniYap();
+//          websitesiniAc();
+//          LoginTestiniYap()
+//      }
+
+    @Test
+    public void driverIslemleriniYap() {
+
+    }
+
+    @Test
+    public void websitesiniAc() {
+
+    }
+
+    @Test
+    public void LoginTestiniYap() {
+
+    }
+
+    @Test
+    public void driverKapat() {
+
+    }
+
 }
