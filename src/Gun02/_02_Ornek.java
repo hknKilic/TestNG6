920a32dac080cae43bb428f7df27ab3e3e6659a5
TechnoStudy <ismettechno@gmail.com>
2023-08-29 17:24:29 +0100

    testNG BaseDriver

--- src/Gun02/_02_Ornek.java
+++ src/Gun02/_02_Ornek.java
@@ -1,7 +1,13 @@
 package Gun02;
 
-public class _02_Ornek {
+import Utlity.BaseDriver;
+import org.testng.annotations.Test;
 
+public class _02_Ornek extends BaseDriver {
 
+    @Test
+    public void test1() {
+        System.out.println("testim çalıştı");
+    }
 
 }
