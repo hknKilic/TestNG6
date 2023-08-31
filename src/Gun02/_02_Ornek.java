672e6c69edc1bd246c1af14aecfe868d5bdd2ffe
TechnoStudy <ismettechno@gmail.com>
2023-08-31 18:15:21 +0100

    _01_SearchFunctionality

--- src/Gun02/_02_Ornek.java
+++ src/Gun02/_02_Ornek.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 public class _02_Ornek extends BaseDriver {
 
     @Test
-    public void loginTest() {
+    public void loginTest() {  // bu bölüm test edileceği zaman BaseDriverdan logintesti isimli metod yorum haline getirilmelidir.
         driver.get("https://opencart.abstracta.us/index.php?route=account/login");
 
         WebElement inputEmail = driver.findElement(By.id("input-email"));
