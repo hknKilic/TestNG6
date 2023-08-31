4908db786f88b366c5a8442e41a48f982eea0048
TechnoStudy <ismettechno@gmail.com>
2023-08-31 19:22:04 +0100

    _03_Soru

---
+++ src/Gun04/_03_Soru.java
@@ -0,0 +1,18 @@
+package Gun04;
+
+
+/*
+   bu örneği istediğin aşamada sorabilirsin, ister burda ister paralel testten sonra
+   Senaryo
+   1- Siteye gidiniz
+   2- Specials tiklayiniz
+   3- Cikan urun lerde (bütün ürünlerde indirim var mı eski fiyat yeni fiyattan büyük mü)
+   4- Yukarıdaki işlemden sonra her ürünün önceki fiyatının şu andaki fiyattan
+      yüksek olduğunu doğrulayınız.
+   5- Yukarıdaki işlemi 2 farklı browserda ve paralel şekilde test ediniz.
+*/
+
+
+public class _03_Soru {
+
+}
