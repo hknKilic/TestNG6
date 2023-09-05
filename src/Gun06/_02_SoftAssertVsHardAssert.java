aebcb0dc18317898fc40569c1539a72206b32fc1
TechnoStudy <ismettechno@gmail.com>
2023-09-05 18:04:54 +0100

    softAssert 2

--- src/Gun06/_02_SoftAssertVsHardAssert.java
+++ src/Gun06/_02_SoftAssertVsHardAssert.java
@@ -2,6 +2,7 @@ package Gun06;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class _02_SoftAssertVsHardAssert {
 
@@ -21,17 +22,20 @@ public class _02_SoftAssertVsHardAssert {
         String strCartPage = "www.facebook.com/cartpage";
         String strEditAccount = "www.facebook.com/editaccountpage";
 
-        Assert.assertEquals("www.facebook.com/homepage", strHomePage); // pass
+        SoftAssert _softAssert=new SoftAssert();
+
+        _softAssert.assertEquals("www.facebook.com/homepage", strHomePage); // pass
         System.out.println("Assert 1");
 
-        Assert.assertEquals("www.facebook.com/profile", strCartPage); // fail
+        _softAssert.assertEquals("www.facebook.com/profile", strCartPage); // fail
         System.out.println("Assert 2");
 
-        Assert.assertEquals("www.facebook.com/homepage", strHomePage); // pass
+        _softAssert.assertEquals("www.facebook.com/settings", strEditAccount); // fail
         System.out.println("Assert 3");
 
-
-
+        _softAssert.assertAll(); // bütün assert sonuçlarını işleme koyuyor.
+        System.out.println("soft assertAll: aktiflik sonrası");
+        // bu bölüm assertAll dan sonra olduğu ve öncesinde hata oluştuğu için ekrana çıkamayacak
     }
 
 }
