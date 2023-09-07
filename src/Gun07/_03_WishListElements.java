e5adb504cb7e458f2d49325f1d68e17bcd42a958
TechnoStudy <ismettechno@gmail.com>
2023-09-07 19:04:25 +0100

    _02_WishList POM XML

--- src/Gun07/_03_WishListElements.java
+++ src/Gun07/_03_WishListElements.java
@@ -1,11 +1,17 @@
 package Gun07;
 
+import Utility.BaseDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 
 public class _03_WishListElements {
+
+    public _03_WishListElements() {
+        PageFactory.initElements(BaseDriver.driver, this);
+    }
 
     @FindBy(xpath="//button[@data-original-title='Add to Wish List']")
     public List<WebElement> wishButtons;
